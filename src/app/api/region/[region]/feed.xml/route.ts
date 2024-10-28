f5ef7c8f1a8995dbcc8b912fa908c325b966930b
Jonah Snider <jonah@jonahsnider.com>
2024-10-28 04:37:30 -0700

    feat: add 6 hour cache lifetime to feed.xml route

--- src/app/api/region/[region]/feed.xml/route.ts
+++ src/app/api/region/[region]/feed.xml/route.ts
@@ -1,8 +1,10 @@
-import { fetchEventListings } from '@/data-fetching/fetcher';
+import { EVENT_LISTING_CACHE_TTL, fetchEventListings } from '@/data-fetching/fetcher';
 import { exceptionRouteWrapper } from '@/exceptions/exception-route-wrapper';
 import { createFeed } from '@/rss/feed-creator';
 import type { NextRouteHandlerSegmentData } from 'next-api-utils';
 import { NextResponse } from 'next/server';
+
+export const revalidate = EVENT_LISTING_CACHE_TTL.to('s');
 
 export const GET = exceptionRouteWrapper.wrapRoute<string, NextRouteHandlerSegmentData<{ region: string }>>(
 	async (request, segmentData) => {
