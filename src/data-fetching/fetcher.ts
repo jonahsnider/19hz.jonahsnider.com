cb89287144f4c947125c32ad53c3443bb015ebe8
Jonah Snider <jonah@jonahsnider.com>
2026-09-14 17:05:27 -0700

    feat: simplify HTTP fetching with fetch-extras
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- src/data-fetching/fetcher.ts
+++ src/data-fetching/fetcher.ts
@@ -1,26 +1,27 @@
 import * as Sentry from '@sentry/astro';
+import { HttpError, pipeline, withHeaders, withHttpError } from 'fetch-extras';
 import { AppException } from '@/exceptions/app-exception';
-import { HTTPError } from '@/exceptions/http-exception';
 import type { EventListing } from './event-listing';
 import { parseEventListingHtml } from './parser';
 
+const fetch19Hz = pipeline(fetch, withHeaders({ 'user-agent': '19hz.jonahsnider.com' }), withHttpError());
+
 export async function fetchEventListings(region: string): Promise<EventListing[]> {
-	const request = new Request(`https://19hz.info/eventlisting_${encodeURIComponent(region)}.php`, {
-		headers: {
-			'user-agent': '19hz.jonahsnider.com',
-		},
-	});
-	const response = await fetch(request);
+	let response: Response;
 
-	if (response.status === 404) {
-		throw new AppException('Unknown region provided', 404);
-	}
+	try {
+		response = await fetch19Hz(`https://19hz.info/eventlisting_${encodeURIComponent(region)}.php`);
+	} catch (error) {
+		if (!(error instanceof HttpError)) {
+			throw error;
+		}
 
-	if (!response.ok) {
-		Sentry.captureException(new HTTPError(response, request));
-		throw new AppException('Failed to fetch event listings', 502, {
-			cause: new HTTPError(response, request),
-		});
+		if (error.response.status === 404) {
+			throw new AppException('Unknown region provided', 404);
+		}
+
+		Sentry.captureException(error);
+		throw new AppException('Failed to fetch event listings', 502, { cause: error });
 	}
 
 	const html = await response.text();
